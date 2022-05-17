439b532cf9f5f7270f570cdbef0f891a0497367d
dataengineer.developer <dataengineer.developer@gmail.com>
2022-05-17 22:34:00 +0100

    novo_estudo_for_while

---
+++ learning_folder/while/6_exec_while.py
@@ -0,0 +1,8 @@
+maior =-1
+nr_digitado = int(input('inseriro um valor positivo: '))
+
+while nr_digitado >=0:
+    if nr_digitado>maior:
+        maior=nr_digitado
+        nr_digitado= int(input('inserir um novo numero positivo: '))
+print('o valor', maior, 'é o maior valor digitado')
